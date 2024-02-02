582b2264b04b139f25ac753b3ab4ae10b5c90001
Francesco Compagno <45362285+kataph@users.noreply.github.com>
2024-02-02 17:36:55 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -10,8 +10,8 @@ const uploadPapersLink = "https://easychair.org/my/conference?conf=fois2024"
 const JOWOlink = "http://iaoa.org/jowo/";
 
 const importantDates = 
-'<li> 17 April 2024 - Paper submission deadline.</li>\n' +
-'<li> 15 May 2024 - Notification of acceptance.</li>\n' +
+'<li> 24 April 2024 - Paper submission deadline.</li>\n' +
+'<li> 22 May 2024 - Notification of acceptance.</li>\n' +
 '<li> 15-19 July 2024 - JOWO workshop days co-located with FOIS 2024 (exact FOMI date TBD).</li>\n' +
 '<li> 30 July 2024 - Camera-ready submission (post-conference proceedings).</li>\n'
 const events = [ // Array of date and text pairs
@@ -19,6 +19,7 @@ const events = [ // Array of date and text pairs
     // { date: "May 27, 2021", text: "We partnered with the Industrial Ontologies Foundry (IOF) to join efforts and present its current activities as part of the FOMI 2021 workshop." },
     // { date: "May 27, 2021", text: "We partnered with the OntoCommons project to join efforts and present a special session on OntoCommons activities as part of the FOMI 2021 workshop." },
     { date: "Gen 15, 2024", text: "FOMI 2024 web site is public." },
+    { date: "Feb 02, 2024", text: "Updated call dates." },
 ];
 const organizers = [
     { name: "Francesco Compagno", email: "francesco.compagno@unitn.it" },
@@ -31,7 +32,7 @@ const committeeList =
     // '<li>Farhad Ameri, Texas State University, USA</li>\n' +
     // '<li>Rebeca Arista Rangel, Airbus, Spain</li>\n' +
     // '<li>Stefano Borgo, ISTC-CNR, Italy</li>\n' +
-	// '<li>Hyunmin Cheong, Autodesk Research, Canada</li>\n' +
+    // '<li>Hyunmin Cheong, Autodesk Research, Canada</li>\n' +
     // '<li>Michael Gruninger, University of Toronto, Canada</li>\n' +
     // '<li>Eeva J&#228;rvenp&#228;&#228;, Tampere University of Technology, Finland</li>\n' +
     // '<li>Hedi Karray, ENIT, France</li>\n' +
@@ -45,14 +46,14 @@ const committeeList =
     // '<li>Jos&#233; Parente De Oliveira, Technological Institute of Aeronautics, Brazil</li>\n' +
     // '<li>Pieter Pauwels, Eindhoven University of Technology, Netherlands</li>\n' +
     // '<li>Mar&#237;a Poveda-Villalon, Universidad Polit&#233;cnica de Madrid, Spain</li>\n' +
-	// '<li>Tiago Prince Sales, Free University of Bozen-Bolzano, Italy</li>\n' +
+    // '<li>Tiago Prince Sales, Free University of Bozen-Bolzano, Italy</li>\n' +
     // '<li>Pedro Rosado Castellano, Universitat Jaume I de Castell&#243;, Spain</li>\n' +
     // '<li>Emilio Sanfilippo, ISTC-CNR, Italy</li>\n' +
     // '<li>Arkopaul Sarkar, ENIT, France</li>\n' +
     // '<li>Georg Schneider, Schaeffler, Germany</li>\n' +
     // '<li>Lorenzo Solano, Polytechnic University of Valencia, Spain</li>\n' +
     // '<li>Daniele Spoladore, STIIMA-CNR, Italy</li>\n' +
-	// '<li>Amanda Vizedom, Credit Suisse, Switzerland</li>\n' +
+    // '<li>Amanda Vizedom, Credit Suisse, Switzerland</li>\n' +
     // '<li>Bob Young, Loughborough University, UK</li>\n'
 
 function createTable() {
